ed97f488b149e739f4c4e9b07eab6dba74c1b6df
Tiago Kuppens <tiago.garcia180@gmail.com>
2021-03-10 21:18:46 -0300

    Concluido

---
+++ Exemplo2Aula1/Exemplo2Aula1/Cachorro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo2Aula1
+{
+    class Cachorro
+    {
+       public string nome;
+       public string raca;
+       public int idade;
+       public string genero; 
+
+        public void AbanarRabo()
+        {
+            Console.WriteLine("Rabinho balançando!!!");
+            Console.ReadLine();
+        }
+        public void Latir()
+        {
+            Console.WriteLine("Au Au Au");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Exemplo2Aula1/Exemplo2Aula1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Exemplo2Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bidu = new Cachorro();
+            bidu.nome = "Bidu Da Silva";
+            bidu.raca = "Pitbull";
+            bidu.genero = "Macho";
+            bidu.idade = 3;
+
+            bidu.Latir();
+            bidu.AbanarRabo();
+        }
+    }
+}
