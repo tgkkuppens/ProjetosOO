b3dc6beb067f5ae456c05f56226dc4618a324121
Tiago Kuppens <tiago.garcia180@gmail.com>
2021-03-10 21:07:57 -0300

    Classe Cachorro, cao1 até cao5, default

---
+++ Exemplo1Aula1/Exemplo1Aula1/Cachorro.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exemplo1Aula1
+{
+    class Cachorro
+    {
+    }
+}
---
+++ Exemplo1Aula1/Exemplo1Aula1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Exemplo1Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cachorro cao1 = new Cachorro();
+            var cao2 = new Cachorro();
+            Cachorro cao3 = new Cachorro();
+            var cao4 = new Cachorro();
+            var cao5 = new Cachorro();
+
+            Cachorro[] cachorros = { cao1, cao2, cao3, cao4, cao5 };
+        }
+    }
+}
