8f6aac5fe51ae0e4b6f066b2abf88cacc45bb31e
Tiago Kuppens <tiago.garcia180@gmail.com>
2021-03-23 22:37:20 -0300

    Contas Bancarias funcionando.

---
+++ Laboratorio02/Laboratorio02/Conta.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Laboratorio02
+{
+    class Conta
+    {
+        string numeroConta;
+        string nomeCorrentista;
+        float saldoAtual;
+        bool contaCorrente;
+
+        public Conta(string numeroConta, string nomeCorrentista, float saldoAtual, bool contaCorrente)
+        {
+            this.numeroConta = numeroConta;
+            this.nomeCorrentista = nomeCorrentista;
+            this.saldoAtual = saldoAtual;
+            this.contaCorrente = contaCorrente;
+        }
+
+        public string NumeroConta { get => numeroConta; }
+        public string NomeCorrentista { get => nomeCorrentista;  }
+        public float SaldoAtual { get => saldoAtual; set => saldoAtual = value; }
+        public bool ContaCorrente { get => contaCorrente;   }
+
+        
+       
+
+
+
+        public void SacarDinheiro(float dinheiroSacado)
+        {
+            float tarifaSaque = 0f;
+            
+            
+            if (contaCorrente)
+            {
+                 tarifaSaque = dinheiroSacado * 0.0037f;
+            }
+            else
+            {
+                 tarifaSaque = dinheiroSacado * 0.002f;
+            }
+
+            if (SaldoAtual < dinheiroSacado + tarifaSaque)
+            {
+                Console.WriteLine("O seu saldo não pode ser negativo.");
+                Console.WriteLine("Operação cancelada.");
+                Console.WriteLine("O seu saldo atual é: R$" + SaldoAtual);
+            }
+            if (SaldoAtual > tarifaSaque + dinheiroSacado)
+            {
+                saldoAtual = saldoAtual - dinheiroSacado - tarifaSaque;
+                Console.WriteLine("Você sacou R$" + dinheiroSacado + ", seu saldo atual é de: R$" + (SaldoAtual - tarifaSaque));
+               
+            }
+            
+        }
+        public void DepositarDinheiro (float dinheiroDepositado)
+        {
+            SaldoAtual = SaldoAtual + dinheiroDepositado;
+            Console.WriteLine("Você depositou R$" + dinheiroDepositado + " seu saldo total é de: R$" + SaldoAtual);
+        }
+
+        public void VerificarSaldo()
+        {
+            Console.WriteLine( nomeCorrentista + ", o seu saldo é de: R$" + SaldoAtual);
+        }
+
+        public void Transferir(float quantidadeTransferida, Conta receptor) 
+        {
+            float taxaTransferencia = 0;
+
+            if (contaCorrente)
+            {
+                taxaTransferencia = quantidadeTransferida * 0.001f;
+            }
+            else
+            {
+                taxaTransferencia = quantidadeTransferida * 0.0015f;
+            }
+
+            if (saldoAtual < quantidadeTransferida + taxaTransferencia)
+            {
+                Console.WriteLine("Saldo indisponível, operação cancelada.");
+            }
+            if (saldoAtual >= quantidadeTransferida + taxaTransferencia)
+            {
+                SaldoAtual -= quantidadeTransferida + taxaTransferencia;
+                receptor.saldoAtual += quantidadeTransferida;
+                Console.WriteLine("Você transferiu R$" + quantidadeTransferida + "para " + receptor.NomeCorrentista);
+            }
+
+        }
+    }
+}
--- Laboratorio02/Laboratorio02/Program.cs
+++ Laboratorio02/Laboratorio02/Program.cs
@@ -6,7 +6,27 @@ namespace Laboratorio02
     {
         static void Main(string[] args)
         {
-            
+            Conta bobNelson = new Conta("12345-6", "Bob Nelson", 0, true);
+            Conta testolfoRocha = new Conta("71717-1", "Testolfo Rocha", 0, false);
+            Conta lisaLeite = new Conta("65432-1", "Lisa Leite", 0, true);
+            bobNelson.DepositarDinheiro(5000f);
+            lisaLeite.DepositarDinheiro(2000f);
+            testolfoRocha.DepositarDinheiro(1500f);
+            bobNelson.Transferir(600, testolfoRocha);
+            lisaLeite.SacarDinheiro(250f);
+            lisaLeite.Transferir(400f, testolfoRocha);
+            testolfoRocha.Transferir(1000f, bobNelson);
+            bobNelson.SacarDinheiro(900f);
+            bobNelson.Transferir(1500f, lisaLeite);
+            testolfoRocha.Transferir(1200f, lisaLeite);
+            bobNelson.SacarDinheiro(2000f);
+            lisaLeite.DepositarDinheiro(100f);
+            testolfoRocha.Transferir(700, bobNelson);
+            bobNelson.VerificarSaldo();
+            testolfoRocha.VerificarSaldo();
+            lisaLeite.VerificarSaldo();
+
+
         }
     }
 }
