939643a12e1f3c2414dfb79a362ab3df0d8eddf7
Tiago Kuppens <tiago.garcia180@gmail.com>
2021-03-27 12:06:53 -0300

    Laboratório 3

--- Exercício3Lab3/Exercício3Lab3/Program.cs
+++ Exercício3Lab3/Exercício3Lab3/Program.cs
@@ -7,7 +7,7 @@ namespace Exercício3Lab3
         static void Main(string[] args)
         {
             //Só deixando claro que não só peguei o código e coloquei aqui sem pensar em nada, 
-            //eu acompanhei o desenvolvimento em sala, por isso deixei os mesmos exemplos para não me gerar confusões.
+            //eu acompanhei o desenvolvimento em sala, por isso deixei os mesmos exemplos para não me gerar confusões. 
             Produto jogoFifa = new JogoDigital("Fifa 2021", 200, "PS5");
             Produto jogoGoW = new JogoDigital("God of War", 5, "PS2");
             Produto livro1 = new Livro("O Príncipe", 40, "Maquiavel");
